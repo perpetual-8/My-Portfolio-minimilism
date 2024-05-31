6a4b979e49abd082f11f862decad915fb07736ca
perpetual-8 <hamzaabhutta@gmail.com>
2024-05-31 06:16:20 -0700

    added project data

--- portfolio/src/components/jsx/Project.jsx
+++ portfolio/src/components/jsx/Project.jsx
@@ -4,6 +4,8 @@ import LeftArrow from "./LeftArrow.svg";
 import RightArrow from "./RightArrow.svg";
 
 import p2img from "../../assets/budget app.png";
+import p1img from "../../assets/weekaway.png";
+import p3img from "../../assets/techbazaar.jpg";
 
 const Aboutme = () => {
   const cardListRef = useRef(null);
@@ -28,16 +30,24 @@ const Aboutme = () => {
         >
           <LeftArrow />{" "}
         </span>
+
         <div className={css.cardList} ref={cardListRef}>
           <div className={css.card}>
-            <p className={css.Pname}> Name </p>
-            <img
-              src="https://via.placeholder.com/300
-"
-              alt=""
-            />
-            <p> 2 line description</p>
-            <p>icon of technology</p>
+            <a
+              href="https://weekaway2.netlify.app/"
+              target="_blank"
+              className={css.link}
+            >
+              <p className={css.Pname}> WEEKAWAY </p>
+              <img src={p1img} alt="" className={css.pImg} />
+              <p>icon of technology</p>
+              <p>
+                {" "}
+                Sleek static website built with React.js and Tailwind CSS,
+                featuring captivating design elements and a single-page layout
+                for effortless navigation and immersive visual appeal.
+              </p>{" "}
+            </a>
           </div>
 
           <div className={css.card}>
@@ -48,26 +58,27 @@ const Aboutme = () => {
             >
               <p className={css.Pname}> Budget Calculator </p>
               <img src={p2img} alt="project 2" className={css.pImg} />
-              <p>
+              <p>icon of technology</p>{" "}
+              <p className={css.desc}>
                 {" "}
                 Kharchaa is a user-friendly home budget calculator designed to
                 help you track your expenses and manage your finances
                 effectively. Its intuitive interface makes budgeting simple and
                 efficient.
               </p>
-              <p>icon of technology</p>{" "}
             </a>
           </div>
 
           <div className={css.card}>
-            <p className={css.Pname}> Name </p>
-            <img
-              src="https://via.placeholder.com/300
-"
-              alt=""
-            />
-            <p> 2 line description</p>
+            <p className={css.Pname}> TechBazaar </p>
+            <img src={p3img} alt="" className={css.pImg} />
             <p>icon of technology</p>
+            <p>
+              {" "}
+              Implemented user authentication, personalized shopping experience,
+              and streamlined checkout process with state management using Redux
+              for efficient data handling.
+            </p>
           </div>
 
           <div className={css.card}>
