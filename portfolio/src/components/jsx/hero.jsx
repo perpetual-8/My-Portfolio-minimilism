1afc6ea1863afb651b65642088079decc662f7f3
perpetual-8 <hamzaabhutta@gmail.com>
2024-06-01 05:08:22 -0700

    added download button

--- portfolio/src/components/jsx/hero.jsx
+++ portfolio/src/components/jsx/hero.jsx
@@ -3,13 +3,27 @@ import css from "../styles/hero.module.scss"
 import ProfileBar from "./ProfileBar"
 
 const Hero = () => {
+
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/Hamza Bhutta FD cv.pdf'; // Updated path to your CV file
+    link.download = 'Hamza Bhutta FD cv.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  
+
   return (
     <div className={css.container}>
 
-
+    
         <p className={css.name}>Hamza Ahmad Bhutta </p>
         <p className={css.role}>Software Engineer </p>
-        <ProfileBar/>
+        <ProfileBar/><button onClick={handleDownload} className={css.button}>
+      Download CV
+    </button>
     </div>
   )
 }
