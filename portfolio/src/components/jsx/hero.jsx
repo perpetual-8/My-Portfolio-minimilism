292be2ff92b5781b59a02db8246132fbf46f3e5c
perpetual-8 <hamzaabhutta@gmail.com>
2024-06-04 12:48:38 -0700

    Added: Text Animation to Name

--- portfolio/src/components/jsx/hero.jsx
+++ portfolio/src/components/jsx/hero.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useRef } from "react";
+import css from "../styles/hero.module.scss";
+import ProfileBar from "./ProfileBar";
+import { gsap } from "gsap";
  
-import css from "../styles/hero.module.scss"
-import ProfileBar from "./ProfileBar"
-
 const Hero = () => {
+   
+  const textRef = useRef(null);
 
+  useEffect(() => {
+    const text = textRef.current;
+    const chars = text.innerText.split('');
 
+    text.innerHTML = chars.map(char => `<span>${char}</span>`).join('');
+
+    gsap.timeline()
+      .fromTo(
+        text.children,
+        { opacity: 0, y: 50 },
+        { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out', stagger: 0.05 }
+      );
+  }, []);
   const handleDownload = () => {
-    const link = document.createElement('a');
-    link.href = '/Hamza Bhutta FD cv.pdf'; // Updated path to your CV file
-    link.download = 'Hamza Bhutta FD cv.pdf';
+    const link = document.createElement("a");
+    link.href = "/Hamza Bhutta FD cv.pdf"; // Updated path to your CV file
+    link.download = "Hamza Bhutta FD cv.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -17,15 +32,15 @@ const Hero = () => {
 
   return (
     <div className={css.container}>
-
-    
-        <p className={css.name}>Hamza Ahmad Bhutta </p>
-        <p className={css.role}>Software Engineer </p>
-        <ProfileBar/><button onClick={handleDownload} className={css.button}>
-      Download CV
-    </button>
+      
+      <p className={css.name} ref={textRef}>Hamza Ahmad Bhutta </p>
+      <p className={css.role}>Software Engineer </p>
+      <ProfileBar />
+      <button onClick={handleDownload} className={css.button}>
+        Download CV
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
